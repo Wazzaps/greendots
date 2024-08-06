bd554957c75901a26c77c2f2e824c1a05a638d6b
David Shlemayev <david.shlemayev@gmail.com>
2024-08-07 00:17:12 +0300

    fix project runs list

--- greendots-frontend/src/components/ProjectRuns.vue
+++ greendots-frontend/src/components/ProjectRuns.vue
@@ -13,9 +13,8 @@ const project_runs = await test_data.getProjectRuns(route.params.project as stri
     <ul>
       <li class="run-row" v-for="run in project_runs" :key="run.id">
         <RouterLink class="run-name" :to="`/${$route.params.project}/${run.id}`">{{
-          run.name
+          run.metadata?.name || run.id
         }}</RouterLink>
-        &nbsp;|&nbsp;
         <span class="run-age" :title="run.created_at">{{ run.pretty_age }}</span>
       </li>
     </ul>
@@ -25,5 +24,16 @@ const project_runs = await test_data.getProjectRuns(route.params.project as stri
 <style scoped>
 main a {
   color: #ffffff;
+}
+.run-age {
+  color: rgb(131, 131, 168);
+}
+ul {
+  padding-inline-start: 1.5em;
+}
+li {
+  list-style: none;
+  display: flex;
+  gap: 12px;
 }
 </style>
